1340c45b8816cfcee4a58c755d3e656c0f4837b7
Luciana Gabriela Valiente <54379062+lucianagabrielavaliente@users.noreply.github.com>
2024-02-06 16:19:54 -0300

    ServicioPrestamos

---
+++ Gestion_de_Bibliotecav2/Servicios/ServicioPrestamos.cs
@@ -0,0 +1,58 @@
+﻿using Gestion_de_Bibliotecav2.DAL;
+using Gestion_de_Bibliotecav2.DAL.EntityFramework;
+using Gestion_de_Bibliotecav2.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_de_Bibliotecav2.Servicios
+{
+    internal class ServicioPrestamos
+    {
+        private RepositorioPrestamos repositorioPrestamos;
+
+        public Prestamo findById(int id)
+        {
+            return repositorioPrestamos.Get(id);
+        }
+
+        public List<Prestamo> findAll()
+        {
+            return (List<Prestamo>)repositorioPrestamos.GetAll();
+        }
+
+        public void Agregar(Prestamo prestamo)
+        {
+            if (prestamo.ID != null && !repositorioPrestamos.Existe(prestamo.ID))
+            { //No debe existir el usuario
+                repositorioPrestamos.Agregar(prestamo);
+            }
+
+            throw new SystemException(); // Si no pasa por el condicional devuelvo un error (sera atrapado por el controlador)
+        }
+
+        public void Actualizar(Prestamo prestamo)
+        {
+            if (prestamo.ID != null && repositorioPrestamos.Existe(prestamo.ID))
+            { //Debe de existir el usuario
+                repositorioPrestamos.Actualizar(prestamo.ID, prestamo);
+            }
+
+            throw new SystemException(); // Si no pasa por el condicional devuelvo un error (sera atrapado por el controlador)
+        }
+
+        public void Eliminar(Prestamo prestamo)
+        {
+            if (prestamo.ID != null && repositorioPrestamos.Existe(prestamo.ID)) //Debe de existir el usuario
+            {
+                repositorioPrestamos.Eliminar(prestamo.ID, prestamo);
+            }
+
+            throw new SystemException(); // Si no pasa por el condicional devuelvo un error (sera atrapado por el controlador)
+        }
+
+    }
+}
+
