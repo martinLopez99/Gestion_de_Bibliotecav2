5853eee6a9d6e0c17faf6bf9bce12f60e7e855d6
Luciana Gabriela Valiente <54379062+lucianagabrielavaliente@users.noreply.github.com>
2024-02-08 16:58:53 -0300

    ControladorPrestamos
    Creación de controlador prestamos.

---
+++ Gestion_de_Bibliotecav2/Controladores/ControladorPrestamos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_de_Bibliotecav2.Controladores
+{
+    internal class ControladorPrestamos
+    {
+    }
+}
