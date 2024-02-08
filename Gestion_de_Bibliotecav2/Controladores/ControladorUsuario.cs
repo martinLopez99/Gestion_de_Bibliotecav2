bfd5d90a4987195c579facfa317b8bbf4cbb0923
Luciana Gabriela Valiente <54379062+lucianagabrielavaliente@users.noreply.github.com>
2024-02-08 19:20:23 -0300

    Controlador Usuario
    Alta, modificar, eliminar, listar y buscar por dni y nombre

--- Gestion_de_Bibliotecav2/Controladores/ControladorUsuario.cs
+++ Gestion_de_Bibliotecav2/Controladores/ControladorUsuario.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Gestion_de_Bibliotecav2.Dominio;
+using Gestion_de_Bibliotecav2.Servicios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,43 @@ namespace Gestion_de_Bibliotecav2.Controladores
 {
     internal class ControladorUsuario
     {
+        ServicioUsuario servicioUsuario;
+        public void CrearUsuario(string dni, string nombre, string direccion, string telefono, string email)
+        {
+            Usuario usuario = new Usuario(int.Parse(dni), nombre, direccion, int.Parse(telefono), email);
+            servicioUsuario.Agregar(usuario);
+        }
+        public void ModificarUsuario(string dni, string nombre, string direccion, string telefono, string email)
+        {
+            Usuario usuario = new Usuario(int.Parse(dni), nombre, direccion, int.Parse(telefono), email);
+            servicioUsuario.Actualizar(usuario);
+        }
+
+        public bool Existe(string dni)
+        {
+           return servicioUsuario.Existe(int.Parse(dni));
+        }
+
+        public Usuario obtenerUsuario(string dniONombre)
+        {
+            Usuario usuario = new Usuario();
+            int number1 = 0;
+            bool canConvert = int.TryParse(dniONombre, out number1);
+            if (canConvert)
+            {
+                usuario = servicioUsuario.obtenerPorDni(int.Parse(dniONombre));
+            }
+            else
+            {
+                // usuario = servicioUsuario.obtenerPorNombre
+            }
+
+            return usuario;
+        }
+
+        public List<Usuario> GetAll()
+        {
+            return servicioUsuario.GetAll();
+        }
     }
 }
