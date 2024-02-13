d7f1673d7d615069cf527794b609a0c68a8be392
fedelederhos5@gmail.com <fedelederhos5@gmail.com>
2024-02-13 17:35:47 +0100

    ContrAutor

---
+++ Gestion_de_Bibliotecav2/Controladores/ControladorAutor.cs
@@ -0,0 +1,26 @@
+﻿using Gestion_de_Bibliotecav2.Dominio;
+using Gestion_de_Bibliotecav2.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_de_Bibliotecav2.Controladores
+{
+    internal class ControladorAutor
+    {
+        ServicioAutor servicioAutor;
+
+        public void CrearAutor(string nombre)
+        {
+            Autor autor = new Autor(nombre);
+            servicioAutor.Agregar(autor);
+        }
+
+        public Autor BuscarAutorPorNombre(string nombre)
+        {
+            return servicioAutor.BuscarAutor(nombre);
+        }
+    }
+}
