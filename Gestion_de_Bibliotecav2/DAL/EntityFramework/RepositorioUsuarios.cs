a0fd1db39833c8d8ae48c3adecc867d3dd68d2c7
Martin Lopez <tincho1597lopez@gmail.com>
2024-02-14 11:37:35 -0300

    Add ExistePorDni

--- Gestion_de_Bibliotecav2/DAL/EntityFramework/RepositorioUsuarios.cs
+++ Gestion_de_Bibliotecav2/DAL/EntityFramework/RepositorioUsuarios.cs
@@ -11,6 +11,18 @@ namespace Gestion_de_Bibliotecav2.DAL.EntityFramework
         {
 
         }
+        public bool ExistePorDni(int dni)
+        {
+            List<Usuario> usuarios = (List<Usuario>)GetAll();
+            foreach (Usuario usuario in usuarios)
+            {
+                if (usuario.DNI == dni)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
         public Usuario obtenerPorDni(int dni)
         {
